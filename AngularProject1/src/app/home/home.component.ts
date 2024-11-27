1ddbb791281236c7482bd9886c0c31a1588517c1
Aleksandar <Aleksandar.Shikov@leadconsult.eu>
2024-11-27 13:05:19 +0200

    new ui

--- AngularProject1/src/app/home/home.component.ts
+++ AngularProject1/src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ofc-home',
-  standalone: false,
-  
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- OfficeBookingWebUI/src/app/app.component.spec.ts
+++ OfficeBookingWebUI/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'OfficeBookingWeb'`, () => {
+  it(`should have as title 'OfficeBookingWebUI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AOfficeBookingWeb');
+    expect(app.title).toEqual('OfficeBookingWebUI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, OfficeBookingWeb');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, OfficeBookingWebUI');
   });
 });
